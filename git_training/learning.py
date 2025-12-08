998820e34194b15981980cf2bfc46f6a15885011
Soumya-Sachidanandam <soumyaparasaram@gmail.com>
2025-12-08 11:51:38 +0530

    adding for loop

--- git_training/learning.py
+++ git_training/learning.py
@@ -1,5 +1,6 @@
-num=int(input("Enter a number: "))
-if num %2==0:
-    print(f'{num} is even')
-else:
-    print(f'{num} is odd')
+for num in range(10,20):
+    num=int(input("Enter a number: "))
+    if num %2==0:
+        print(f'{num} is even')
+    else:
+        print(f'{num} is odd')
