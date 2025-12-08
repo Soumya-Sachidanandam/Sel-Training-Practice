738cba69d693b2dd873510319ae2925bb2b707e8
Soumya-Sachidanandam <soumyaparasaram@gmail.com>
2025-12-08 11:57:05 +0530

    defined function

--- git_training/learning.py
+++ git_training/learning.py
@@ -1,6 +1,10 @@
-for num in range(10,20):
-    num=int(input("Enter a number: "))
-    if num %2==0:
-        print(f'{num} is even')
-    else:
-        print(f'{num} is odd')
+
+def even_odd(start,end):
+    for num in range(10,20):
+        num=int(input("Enter a number: "))
+        if num %2==0:
+            print(f'{num} is even')
+        else:
+            print(f'{num} is odd')
+
+even_odd(10,20)
