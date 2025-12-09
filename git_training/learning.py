cecf8d9995a5734ccf2e964219693777b8ba7f76
Soumya-Sachidanandam <soumyaparasaram@gmail.com>
2025-12-09 10:32:55 +0530

    created Numbers class

--- git_training/learning.py
+++ git_training/learning.py
@@ -1,10 +1,11 @@
+class Numbers:
+    def even_odd(self,start,end):
+        for num in range(10,20):
+            num=int(input("Enter a number: "))
+            if num %2==0:
+                print(f'{num} is even')
+            else:
+                print(f'{num} is odd')
 
-def even_odd(start,end):
-    for num in range(10,20):
-        num=int(input("Enter a number: "))
-        if num %2==0:
-            print(f'{num} is even')
-        else:
-            print(f'{num} is odd')
-
-even_odd(10,20)
+num=Numbers()
+num.even_odd(10,20)
